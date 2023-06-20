527ce393fa9038d420b20a88b4dc5fbb9cababf8
FedeOstro <ferdferdostro@gmail.com>
2023-06-20 20:57:59 -0300

    Faltaun poco

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,22 +5,13 @@ namespace ScapeRoom.Controllers;
 
 public class HomeController : Controller
 {
-      private Maze maze;
-       public HomeController()
-        {
-            // Inicializar la instancia de Maze en el constructor
-            maze = new Maze(15, 10);
-            maze.GenerateMaze();
-        }
+
+
         public ActionResult Index()
         {
             return View();
         }
 
-      public ActionResult Habitacion1()
-        {
-           return View("habitacion1", maze); // Pasar la instancia de Maze a la vista "habitacion1.cshtml"
-        }
     public IActionResult Tutorial(){
         return View();
     }
@@ -36,20 +27,6 @@ public class HomeController : Controller
     public IActionResult Habitacion(int NumSala){
         return View("Habitacion"+NumSala);
     }
-    [HttpPost]
-        public ActionResult CheckCollision(int row, int col)
-        {
-            bool collision = maze.CheckCollision(row, col);
-            return Json(new { collision });
-        }
-        [HttpPost]
-        public ActionResult CheckEnd(int row, int col)
-        {
-            bool end = maze.CheckEnd(row, col);
-            return Json(new { end });
-        }
-
-        [HttpPost]
     public IActionResult ResolverHabitacion(int sala, string clave){
         bool resolvio = escape.ResolverSala(sala, clave);
         if(resolvio == true){
